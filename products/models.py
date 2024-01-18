bac28363a34a9596ff87cffa2ad7e1d89aae7b91
RadwanMaazon <redwan30503321@gmail.com>
2024-01-18 23:54:05 +0200

    Add product table to mycoffeedb as a model class

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+class product(models.Model):
+    name        = models.CharField(max_length=150)
+    description = models.TextField()
+    price       = models.DecimalField(max_digits=6, decimal_places=2)
+    photo       = models.ImageField(upload_to='photo/%Y/%m/%d/')
+    is_active   = models.BooleanField( default=True )
+    publish_date= models.DateTimeField(default= datetime.now)
+    def __str__(self):
+        return self.name 
+    
